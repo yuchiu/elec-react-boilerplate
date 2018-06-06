0c0fcc5998c5dfc79a3e2e3b6a916e16803cdb71
Yu Chiu <yuchiu2002@hotmail.com>
2018-06-06 17:00:40 -0400

    setup webpack and react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+const app = (
+    <Home/>
+)
+ReactDOM.render(app, document.getElementById('root'))
+
 console.log('sdadsasadsdads')
